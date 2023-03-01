2166be93e129aae2ff6c13e0ee19a308819e633a
Theodore Huppert <huppert1@pitt.edu>
2023-03-01 18:01:56 -0500

    initial

---
+++ OpenGL_NIRS/OpenGL_NIRS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenGL_NIRS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
